aad9c6d89bb16e06dbe054f6bfe6c3cdd4f1fb0c
YergaliKumisbay <kumisbayyergali@gmail.com>
2025-02-14 19:09:20 +0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <h1>Сәлем, әлем!</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
